e8de8fa91b35b0cdb737038c0a9b9892eb9b2d7f
Martin Chorley <martin.chorley@gmail.com>
2012-03-09 14:50:50 +0000

    settings bugfix

--- src/django/lastvis/settings.py
+++ src/django/lastvis/settings.py
@@ -76,7 +76,7 @@ ADMIN_MEDIA_PREFIX = '/static/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(ROOT_PATH, 'common/static')
+    os.path.join(ROOT_PATH, 'common/static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
