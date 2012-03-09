892f0cd847d09e3573ab1a18ee159514a2d234c7
Martin Chorley <martin.chorley@gmail.com>
2012-03-09 14:59:59 +0000

    remove age from lastfmuser

--- src/django/lastvis/lastfmauth/models.py
+++ src/django/lastvis/lastfmauth/models.py
@@ -7,5 +7,4 @@ class LastFMUser( models.Model ):
     user = models.ForeignKey( User, unique=True )
     url = models.CharField( max_length=200, blank=True, null=True )
     image = models.CharField( max_length=200, blank=True, null=True )
-    age = models.IntegerField( blank=True, null=True)
     playcount = models.IntegerField( blank=True, null=True )
