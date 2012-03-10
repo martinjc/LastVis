988e08db3a9f5a3a000ddc802915051a709acf96
Martin Chorley <martin.chorley@gmail.com>
2012-03-10 17:29:40 +0000

    more errors

--- src/django/lastvis/lastfmdata/views.py
+++ src/django/lastvis/lastfmdata/views.py
@@ -122,6 +122,7 @@ def return_chart_data( request, api, user, weeks, ):
 
         for artist in artist_totals:
             artist_info = api.artist_getinfo( artist = artist['name'] )
+            logger.info( artist_info.to_dict() )
             artist_genre = artist_info.tags.tag[0].name;
 
             this_genre = None
