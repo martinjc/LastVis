b6a383a20033262339270d657cdf85cfb941018d
Martin Chorley <martin.chorley@gmail.com>
2012-03-10 15:54:45 +0000

    updated to return real chart

--- src/django/lastvis/lastfmdata/test_json.py
+++ src/django/lastvis/lastfmdata/test_json.py
@@ -1,28 +1,93 @@
-chart = { "genres" : [
-    { "name": "Rock", "playcount": 158, "artists" : [
-        { "name" : "Some Artist 3", "playcount": 80, "tracks": [
-                { "title" : "Track 1", "playcount" : 45 },
-                { "title" : "Track 2", "playcount" : 23 },
-                { "title" : "Track 3", "playcount" : 12 }] },
-            { "name" : "Some Artist 2", "playcount": 40, "tracks": [
-                { "title" : "Track 1", "playcount" : 24 },
-                { "title" : "Track 2", "playcount" : 13 },
-                { "title" : "Track 3", "playcount" : 3 }] },
-            { "name" : "Some Artist 1", "playcount": 38, "tracks": [
-                { "title" : "Track 1", "playcount" : 16 },
-                { "title" : "Track 2", "playcount" : 13 },
-                { "title" : "Track 3", "playcount" : 8 }] }] },
-        { "name": "Metal", "playcount": 61, "artists" : [
-            { "name" : "Some Artist 4", "playcount": 34, "tracks": [
-                { "title" : "Track 1", "playcount" : 20 },
-                { "title" : "Track 2", "playcount" : 11 },
-                { "title" : "Track 3", "playcount" : 3 }] },
-            { "name" : "Some Artist 6", "playcount": 15, "tracks": [
-                { "title" : "Track 1", "playcount" : 8 },
-                { "title" : "Track 2", "playcount" : 6 },
-                { "title" : "Track 3", "playcount" : 1 }] },
-            { "name" : "Some Artist 5", "playcount": 12, "tracks": [
-                { "title" : "Track 1", "playcount" : 6 },
-                { "title" : "Track 2", "playcount" : 4 },
-                { "title" : "Track 3", "playcount" : 2 }] }] }  ] }
+chart = { "genres": [
+    {"playcount": 22, "name": "grunge", "artists": [
+        {"tracks": [
+            {"playcount": 1, "name": "Breed"},
+            {"playcount": 1, "name": "Come as You Are"},
+            {"playcount": 1, "name": "Drain You"},
+            {"playcount": 1, "name": "In Bloom"},
+            {"playcount": 1, "name": "Lithium"},
+            {"playcount": 1, "name": "Lounge Act"},
+            {"playcount": 1, "name": "On A Plain"},
+            {"playcount": 1, "name": "Smells Like Teen Spirit"},
+            {"playcount": 1, "name": "Something In The Way"},
+            {"playcount": 1, "name": "Stay Away"},
+            {"playcount": 1, "name": "Territorial Pissings"},
+            {"playcount": 1, "name": "Pennyroyal Tea"},
+            {"playcount": 1, "name": "Milk It"},
+            {"playcount": 1, "name": "Tourette's"},
+            {"playcount": 1, "name": "Radio Friendly Unit Shifter"}], "playcount": 16, "name": "Nirvana"},
+        {"tracks": [
+            {"playcount": 1, "name": "Fell On Black Days"},
+            {"playcount": 1, "name": "Head Down"},
+            {"playcount": 1, "name": "Let Me Drown"},
+            {"playcount": 1, "name": "Mailman"},
+            {"playcount": 1, "name": "My Wave"},
+            {"playcount": 1, "name": "Superunknown"}], "playcount": 6, "name": "Soundgarden"}
+    ]},
+    {"playcount": 2, "name": "indie", "artists": [
+        {"tracks": [
+            {"playcount": 1, "name": "Novocaine For The Soul"},
+            {"playcount": 1, "name": "Susan's House"}], "playcount": 2, "name": "Eels"}
+    ]},
+    {"playcount": 16, "name": "punk", "artists": [
+        {"tracks": [
+            {"playcount": 1, "name": "Bottles To The Ground"},
+            {"playcount": 1, "name": "Dinosaurs Will Die"},
+            {"playcount": 1, "name": "Herojuana"},
+            {"playcount": 1, "name": "Louise"},
+            {"playcount": 1, "name": "My Vagina"},
+            {"playcount": 1, "name": "Pharmacist's Daughter"},
+            {"playcount": 1, "name": "Thank God It's Monday"},
+            {"playcount": 1, "name": "Theme From a NOFX Album"},
+            {"playcount": 1, "name": "Total Bummer"},
+            {"playcount": 1, "name": "Kids of the K-Hole"},
+            {"playcount": 1, "name": "And Now for Something Completely Similar"},
+            {"playcount": 1, "name": "Clams Have Feelings Too"},
+            {"playcount": 1, "name": "Take Two Placebos and Call Ma Lame"},
+            {"playcount": 1, "name": "Stranger Than Fishin'"},
+            {"playcount": 1, "name": "What's the Matter With Parents Today?"},
+            {"playcount": 1, "name": "Bath of Least Resistance [#]"}], "playcount": 16, "name": "NOFX"}
+    ]}, {"playcount": 2, "name": "thrash metal", "artists": [
+        {"tracks": [
+            {"playcount": 1, "name": "Some Kind Of Monster"},
+            {"playcount": 1, "name": "Dirty Window"}], "playcount": 2, "name": "Metallica"}
+    ]}, {"playcount": 28, "name": "rock", "artists": [
+        {"tracks": [
+            {"playcount": 1, "name": "Exhausted"},
+            {"playcount": 1, "name": "All My Life"},
+            {"playcount": 1, "name": "Low"},
+            {"playcount": 1, "name": "Have It All"},
+            {"playcount": 1, "name": "Tired Of You"},
+            {"playcount": 1, "name": "Halo"},
+            {"playcount": 1, "name": "Overdrive"},
+            {"playcount": 1, "name": "X-Static"},
+            {"playcount": 1, "name": "Floaty"},
+            {"playcount": 1, "name": "Big Me"},
+            {"playcount": 1, "name": "This Is A Call"},
+            {"playcount": 1, "name": "Weenie Beenie"},
+            {"playcount": 1, "name": "For All The Cows"},
+            {"playcount": 1, "name": "Good Grief"},
+            {"playcount": 1, "name": "Oh, George"},
+            {"playcount": 1, "name": "Disenchanted Lullaby"},
+            {"playcount": 1, "name": "Times Like These"},
+            {"playcount": 1, "name": "I'll Stick Around"},
+            {"playcount": 1, "name": "Wattershed"},
+            {"playcount": 1, "name": "Lonely As You"},
+            {"playcount": 1, "name": "Have a Cigar"},
+            {"playcount": 1, "name": "Iron and Stone"},
+            {"playcount": 1, "name": "Alone + Easy Target"}], "playcount": 23, "name": "Foo Fighters"},
+        {"tracks": [
+            {"playcount": 1, "name": "Somebody To Shove"},
+            {"playcount": 1, "name": "Just Like Anyone"},
+            {"playcount": 1, "name": "Close"},
+            {"playcount": 1, "name": "Cartoon"},
+            {"playcount": 1, "name": "Closer To The Stars [Live]"}], "playcount": 5, "name": "Soul Asylum"}
+    ]}, {"playcount": 1, "name": "mysterious", "artists": [
+        {"tracks": [], "playcount": 1, "name": "Original Soundtrack"}]}]}
+
+
+
+
+
+
 
