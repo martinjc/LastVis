e7d18175267e202ec595fd43baa03e2ee3e3c5cf
Martin Chorley <martin.chorley@gmail.com>
2012-03-11 10:18:18 +0000

    jitcharts

--- src/django/lastvis/base/urls.py
+++ src/django/lastvis/base/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns( 'base.views',
     # main page
     url( r'^processing/$', view = processing_test, name = 'base_processing' ),
 
+    url( r'^jitcharts/$', view = jit_charts, name = 'jit_charts' ),
 
     url( r'^weeklycharts/$', view = weekly_charts, name = 'weekly_charts' ),
  )
